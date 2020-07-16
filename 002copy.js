fc09a840cb7f0ca1f1c07c38a5b396a736d96116
sadrakss <sadrakss@outlook.com.br>
2020-07-16 13:27:47 -0300

    First Challenge Code Wars

---
+++ 002copy.js
@@ -0,0 +1,29 @@
+console.log(SquareDigits(90024));
+
+function SquareDigits(num) {
+   let createArray = CreateArray(num);
+   return ArrayPower(createArray);
+}
+
+function ArrayPower(Array) {
+   let arraySize = Array.length;
+   let array = Array;
+
+   let string = [];
+   for (i = 0; i < arraySize; i++) {
+      string += parseInt(array[i] * array[i])
+   }
+   return parseInt(string);
+}
+
+function CreateArray(num) {
+   let string = [];
+   let cont = 0;
+   while (num > 0) {
+      string[cont] = parseInt(num % 10);
+      num = parseInt(num / 10);
+      cont++;
+   }
+   return string.reverse();
+}
+
